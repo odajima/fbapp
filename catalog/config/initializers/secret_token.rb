e1867e6c519411741e35009642002267d6b92e43
odajima <odajima0315@gmail.com>
2012-07-16 12:25:48 +0900

    heroku sample

---
+++ catalog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = '35267b08fabc3ddbc82c339d1e6e42ab95fecb72617f0a05aa9dae9ff13d445231849b3932cb31461af8e392e58a7132acae2d9cd3f1e1e0405a845e72fd54f5'
